f42ef456c71db1e105db4c800530bbe7f7cf0f55
Aaron Schulman <shalevaaron@gmail.com>
2024-10-16 09:44:58 -0700

    class for today

---
+++ ptrs_and_arrays.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  char ff_char = 0xFF;
+  char* ff_char_ptr = &ff_char;
+  char ff_arr[] = { 0xFF };
+  char ff_str[] = "ff";
+
+  printf("ff_char (as one byte hex): 0x%hhx\n", ff_char);
+  printf("*ff_char_ptr (as one byte hex): 0x%hhx\n", ff_char_ptr);
+  printf("ff_arr (as one byte hex): 0x%hhx\n", ff_str);
+  printf("ff_str (as one byte hex): 0x%hhx\n", ff_arr);
+  printf("ff_arr[0] (as one byte hex): 0x%hhx\n", ff_str[0]);
+  printf("ff_str[0] (as one byte hex): 0x%hhx\n", ff_arr[0]);
+
+  printf("ff_char_ptr (as pointer): 0x%p\n", &ff_char);
+  printf("ff_arr (as pointer): 0x%p\n", ff_str);
+  printf("ff_str (as pointer): 0x%p\n", ff_arr);
+
+  return 0;
+}
